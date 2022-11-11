8d90adcfaa4fc59b4a9729769c5975b53a9c6207
Larkin <ab88199594ae@mail.ru>
2022-11-11 16:53:05 +0400

    alphabet

--- ConsoleProject.cpp
+++ ConsoleProject.cpp
@@ -5,10 +5,14 @@
 
 #include <tchar.h>
 #include <stdio.h>
+#include <conio.h>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	AnsiString dd;
 	UnitellerID d;
+	//const static char da[3] = "DAz";
+	//printf("%s %s\n",da,UnitellerID::Alphabet);
+	getch();
 	return 0;
 }
--- UnitellerClass.hpp
+++ UnitellerClass.hpp
@@ -1,13 +1,34 @@
 #include <tchar.h>
 
+typedef char TUID[2];
+
 class UnitellerID
 {
-	private:
+private:
+	TUID FID;
+	static char Alphabet[26];
 
-	protected:
+protected:
 
-	public:
-		__fastcall UnitellerID();
-		__fastcall ~UnitellerID();
+public:
+	__fastcall UnitellerID();
+	__fastcall ~UnitellerID();
+	static int i;	
 	char ID[2];
-};                                        
+};            
+
+int UnitellerID::i = 2;
+//В идентификаторах никогда не должны присутствовать буквы «D», «F», «G», «J», «M», «Q», «V» и цифра «0».
+char UnitellerID::Alphabet[] = "ABCEHIKLNOPRSTUWXYZ";
+
+__fastcall UnitellerID::UnitellerID()
+{
+ 
+}
+
+__fastcall UnitellerID::~UnitellerID()
+{
+ 
+}
+
+				
