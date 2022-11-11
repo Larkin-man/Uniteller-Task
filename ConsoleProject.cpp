dcc6a9d2a51354a9b0c65be3c6bcd83b08243c30
Larkin <ab88199594ae@mail.ru>
2022-11-11 16:07:40 +0400

    Initial commit

---
+++ ConsoleProject.cpp
@@ -0,0 +1,14 @@
+#include <System.hpp>
+#include "UnitellerClass.hpp"
+#pragma hdrstop
+#pragma argsused
+
+#include <tchar.h>
+#include <stdio.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	AnsiString dd;
+	UnitellerID d;
+	return 0;
+}
