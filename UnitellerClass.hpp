20c2dc781b4415bc03654fe3930d9a9b7e024ec3
Larkin <ab88199594ae@mail.ru>
2022-11-11 16:09:46 +0400

    delete debug

---
+++ UnitellerClass.hpp
@@ -0,0 +1,13 @@
+#include <tchar.h>
+
+class UnitellerID
+{
+	private:
+
+	protected:
+
+	public:
+		__fastcall UnitellerID();
+		__fastcall ~UnitellerID();
+	char ID[2];
+};                                        
