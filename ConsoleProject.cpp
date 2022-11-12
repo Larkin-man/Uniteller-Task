c51d6ef37ed3d742153bddfbcf48352c5830175d
Larkin <ab88199594ae@mail.ru>
2022-11-12 16:48:58 +0400

    SetID done

--- ConsoleProject.cpp
+++ ConsoleProject.cpp
@@ -1,4 +1,3 @@
-#include <System.hpp>
 #include "UnitellerClass.hpp"
 #pragma hdrstop
 #pragma argsused
@@ -9,27 +8,20 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	AnsiString dd;
 	UnitellerID d;
 	//printf("%s %s\n",da,UnitellerID::Alphabet);
 	int i;
-	for (i = 0; i < 25; i++)
-	{
-		printf("%d:%s;%d\n",i,d.SetIndex(i),d.GetIndex(d.SetIndex(i)));
-	}
-	printf("%d=A1 ",d.SetID("A1"));
-	printf("%d=B2 ",d.SetID("B2"));
-	printf("%d=Z9 ",d.SetID("Z9"));
-	printf("%d=A1-A1 ",d.SetID("A1-A1"));
-	printf("%d=A1-A2 ",d.SetID("A1-A2"));
-	printf("%d=A2-A1 ",d.SetID("A2-A1"));
-	printf("%d=A1-A1-A1 ",d.SetID("A1-A1-A1"));
-	unsigned long int p = 1;
-	for (i = 0; i < 10; i++)
-	{
-		p *=171;
-		printf("%d ",p);
-	}
+	//for (i = 0; i < 25; i++)
+	//	printf("%d:%s;%d\n",i,d.SetIndex(i),d.GetIndex(d.SetIndex(i)));
+	d.SetID("V7");
+	printf("V7\n%s<<%d ",d.FIDS,d.Length);
+
+	d.SetID("G1-A5-H9");
+	printf("G1-A5-H9=%d\n",d.Length);
+	printf("%s<<\n",d.FIDS);
+	d.SetID("G1-A5-H9-I2-J3-K4-L5-N6-M7-O8DFSFS");
+	printf("\nG1-A5-H9-I2-J3-K4-L5-N6-M7-O8DFSFS=%d",d.Length);
+	printf("%s<<\n",d.FIDS);
 	getch();
 	return 0;
 }
--- UnitellerClass.hpp
+++ UnitellerClass.hpp
@@ -12,37 +12,37 @@ typedef char TUID[2];
 
 class UnitellerID
 {
-private:
+protected:
 	TUID FID;
 	static char Alphabet[ENABLECHARS];
+	static char Disabled[DISABLECHARS];
 	int FIndex;
-	char FIDS[IDSLEN]; //nullterm
-
-protected:
-	int GetPos(char symb)
-	{
-		int i = 0;
-		for (; i < ENABLECHARS; i++)
-		if (Alphabet[i] == symb)
-			return i;
-		return i;
-    }
+	int GetPos(char symb);
+	bool IfDisabled(char symb);
 
 public:
 	__fastcall UnitellerID();
-	__fastcall ~UnitellerID();	
-	char ID[2];
+	//__fastcall ~UnitellerID();
 	int GetIndex(TUID ID1);
 	char* SetIndex(int Index);
-	int SetID(char *ID); //NULL TEMINAtED StriNg
+	void SetID(char *ID); //NULL TEMINAtED StriNg
+	char FIDS[IDSLEN]; //nullterm
+	int Length;
 };            
 
-//Â èäåíòèôèêàòîðàõ íèêîãäà íå äîëæíû ïðèñóòñòâîâàòü áóêâû «D», «F», «G», «J», «M», «Q», «V» è öèôðà «0».
+//Â èäåíòèôèêàòîðàõ íèêîãäà íå äîëæíû ïðèñóòñòâîâàòü
+//áóêâû «D», «F», «G», «J», «M», «Q», «V» è öèôðà «0».
 char UnitellerID::Alphabet[] = "ABCEHIKLNOPRSTUWXYZ";
+char UnitellerID::Disabled[] = "DFGJMQV";
 
-__fastcall UnitellerID::UnitellerID() { FIDS[IDSLEN-1] = '\0'; }
+__fastcall UnitellerID::UnitellerID()
+{
+	for (int i = 0; i < IDSLEN; i++)
+		FIDS[i] = '\0';
+    Length = 0;
+}
 
-__fastcall UnitellerID::~UnitellerID() { }
+//__fastcall UnitellerID::~UnitellerID() { }
 
 int UnitellerID::GetIndex(TUID ID1)
 {
@@ -64,32 +64,51 @@ char* UnitellerID::SetIndex(int Index)
 	return FID;
 }
 
-int UnitellerID::SetID(char *ID)
+void UnitellerID::SetID(char *ID)
 {
 	int i = 0;
-	int idx = 0;
-	for (i = 0; i < ALLBLOCKS; i+=3)
-	{
+	for (i = 0; i < IDSLEN; i += 3)
 		if (ID[i] >= 'A' && ID[i] <= 'Z')
 		{
-			idx += GetPos(ID[i]) * ALLNUMS;
-			if (ID[i+1] >= '1' && ID[i+1] <= '9' )
+			while (IfDisabled(ID[i]))
+				ID[i] = ID[i] - 1;
+			if (ID[i+1] >= '1' && ID[i+1] <= '9')
 			{
-				idx += ID[i+1] - '1';
-				if (ID[i+2] == '-')
+				if (ID[i+2] != '-')
 				{
-					idx = (idx+1) * BLOCKMULT;
-				}
-				else
+					i += 3;
 					break;
+				}
 			}
 			else
 				break;
 		}
 		else
 			break;
+	if (i > 0)
+	{
+		FIDS[i-1] = '\0';
+		i -= 2;
+		Length = i;
+		for (; i >= 0; i--)
+			FIDS[i] = ID[i];
 	}
-	return idx;
 }
 
-				
+int UnitellerID::GetPos(char symb)
+{
+	int i = 0;
+	for (; i < ENABLECHARS; i++)
+	if (Alphabet[i] == symb)
+		return i;
+	return i;
+}
+
+bool UnitellerID::IfDisabled(char symb)
+{
+	int i = 0;
+	for (; i < DISABLECHARS; i++)
+	if (Disabled[i] == symb)
+		return true;
+	return false;
+}
